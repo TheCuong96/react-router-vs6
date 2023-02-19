78822429882ceca6c3c4cc105df5e0fb9e7d12ce
thecuong1396 <cuongphan@coinmap.tech>
2023-02-20 05:57:11 +0700

    handle width tag Link and NavLink

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 interface Props {
   children?: React.ReactNode
 }
@@ -10,28 +10,50 @@ export default function MainLayout({ children }: Props) {
         <div className='h-full overflow-y-auto bg-gray-100 py-4 px-3 shadow-lg'>
           <ul className='space-y-2'>
             <li>
-              <Link
+              <NavLink
                 to='/'
-                className='flex items-center rounded-lg bg-gray-300 p-2 text-base font-normal text-gray-900 hover:bg-gray-300'
+                end // nó giống exact, dùng để xác định xem thằng cha đã match hay chưa, nếu match rồi thì thằng khác sẽ không match nữa, ngắn gọn là nó dùng để xác thực đúng 1 thằng vào 1 thời điểm được match
+                replace // mặc định khi ta đổi trang, thì nó sẽ lưu vào history page trong trình duyệt của chúng ta và thằng này dùng để ngăn chặn điều đó với chính bản thân nó, khi có nó thì nó sẽ không lưu vào lịch sử duyệt web của link này mà nó lấy url hiện tại để ghi đè lên
+                className={({ isActive }) => {
+                  const isActiveClass = isActive ? 'bg-gray-300  ' : ''
+                  return `flex items-center rounded-lg ${isActiveClass} p-2 text-base font-normal text-gray-900 hover:bg-gray-300 `
+                }} // isActive được sinh ra bởi react-router-dom và dành riêng cho thẻ NavLink này khi nó được chọn, nó sẽ true giá trị của isActive lên và ta dựa vào đó để handle css hoặc điều gì đó khác...
+
+                // style={({ isActive }) => ({
+                //   fontWeight: isActive ? 800 : undefined
+                // })} // ngoài cách trên thì vẫn có thể dùng như vậy
               >
-                <span className='ml-3 font-bold'>Dashboard</span>
-              </Link>
+                {/* hoặc ta có thể xử lý ở dưới children như vậy với isActive */}
+                {({ isActive }) => {
+                  return <span className={`ml-3 ${isActive ? 'font-bold' : ''}`}>Dashboard</span>
+                }}
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to='/staff'
-                className='flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-300'
+                className={({ isActive }) => {
+                  const isActiveClass = isActive ? 'bg-gray-300' : ''
+                  return `flex items-center rounded-lg ${isActiveClass} p-2 text-base font-normal text-gray-900 hover:bg-gray-300 `
+                }}
               >
-                <span className='ml-3'>Staff</span>
-              </Link>
+                {({ isActive }) => {
+                  return <span className={`ml-3 ${isActive ? 'font-bold' : ''}`}>Staff</span>
+                }}
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to='/about'
-                className='flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-300'
+                className={({ isActive }) => {
+                  const isActiveClass = isActive ? 'bg-gray-300' : ''
+                  return `flex items-center rounded-lg ${isActiveClass} p-2 text-base font-normal text-gray-900 hover:bg-gray-300 `
+                }}
               >
-                <span className='ml-3'>About</span>
-              </Link>
+                {({ isActive }) => {
+                  return <span className={`ml-3 ${isActive ? 'font-bold' : ''}`}>About</span>
+                }}
+              </NavLink>
             </li>
           </ul>
         </div>
