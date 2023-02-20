9c81cba5f3092c007821d6f750b78076e7db4398
thecuong1396 <cuongphan@coinmap.tech>
2023-02-21 02:03:58 +0700

    xử lý điều hướng cơ bản khi vào bị vào trang not found page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,12 @@
+import { useLocation } from 'react-router-dom'
+
 export default function Dashboard() {
+  const location = useLocation()
+  console.log('location', location)
   return (
     <div>
       <h1 className='mb-6 text-lg'>Dashboard</h1>
+      <div>{location.state}</div>
     </div>
   )
 }
--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 export default function NotFound() {
-  return <div>NotFound</div>
+  // 2 cách để xử lý điều hướng với nagigate của react-router-dom
+  const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/', { replace: true, state: 'bạn đã được điều hướng từ not found page sang trang chủ' })
+    }, 2000)
+  }, [navigate])
+
+  return (
+    <>
+      {/* <Navigate to='/' /> */}
+      <div>Not Found</div>
+    </>
+  )
 }
